648bd2930cb4d9a45f6ec96c3794ce7274937594
odani <o.tetsuya26@gmail.com>
2022-01-21 20:19:08 +0900

    prettier設定を追加

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  semi: false,
+}
